73d477a6b68183a9eff82dcd30bfd6198d07ba3a
Marilynn Stone <hello@marilynnstone.com>
2022-08-01 00:15:33 +0000

    Add assignments and assignment_submissions tables to database.

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,16 @@
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT Null,
+  name VARCHAR(255) NOT Null,
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
